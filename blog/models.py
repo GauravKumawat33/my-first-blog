d75903f68c8c897a40617b2cde428e2a390f5159
GauravKumawat33 <gauravkumawat4363@gmail.com>
2021-11-03 00:39:02 +0530

    init

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
+from . import timezone
 
 class Post(models.Model):
     author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
